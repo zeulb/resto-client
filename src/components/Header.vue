0c00388c3334d070d87ad298c9c2e0dc5b0b2bd8
Stefano Chiesa <base278@gmail.com>
2017-11-04 00:19:47 +0800

    Add icon to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="Header">
+    <icon class="Header-icon" name="cutlery"></icon>
     <span class="Header-title">Resto</span>
   </div>
 </template>
 
 <script>
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/cutlery'
+
 export default {
   name: 'Header',
+  components: {
+    Icon
+  },
   data() {
     return {}
   }
@@ -15,6 +22,8 @@ export default {
 
 <style scoped>
 .Header {
+  display: flex;
+  align-items: center;
   padding: 10px 15vw;
   background-color: #f57347;
 }
@@ -23,5 +32,14 @@ export default {
   font-size: xx-large;
   font-weight: 700;
   color: white;
+}
+
+.Header-icon {
+  color: white;
+  width: auto;
+  height: 2em;
+  max-width: 100%;
+  max-height: 100%;
+  margin-right: 15px;
 }
 </style>
